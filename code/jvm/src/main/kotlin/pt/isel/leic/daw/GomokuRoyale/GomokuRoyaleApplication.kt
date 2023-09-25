19434c76de989c9439142b3147ef6b435f77f610
RicardoPinto26 <a47673@alunos.isel.pt>
2023-09-25 10:52:52 +0100

    Adds Example Spring Project

---
+++ code/jvm/src/main/kotlin/pt/isel/leic/daw/GomokuRoyale/GomokuRoyaleApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.leic.daw.GomokuRoyale
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GomokuRoyaleApplication
+
+fun main(args: Array<String>) {
+	runApplication<GomokuRoyaleApplication>(*args)
+}
---
+++ code/jvm/src/test/kotlin/pt/isel/leic/daw/GomokuRoyale/GomokuRoyaleApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.isel.leic.daw.GomokuRoyale
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GomokuRoyaleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
