749de2a28e0f033f76eed0311cbac7071d0eea47
Francisco Medeiros <91284180+Francisco-Medeiros00@users.noreply.github.com>
2023-10-09 12:37:47 +0100

    Changes by Luis

--- code/jvm/src/main/kotlin/pt/isel/leic/daw/gomokuRoyale/controllers/UsersController.kt
+++ code/jvm/src/main/kotlin/pt/isel/leic/daw/gomokuRoyale/controllers/UsersController.kt
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/users")
-class UsersController {
+class UsersController(
+    private val userService: UsersService
+) {
 
     @PostMapping("/create")
     fun createUser() {
@@ -22,5 +24,26 @@ class UsersController {
     @PostMapping("/tokens")
     fun createToken() {
         TODO()
+    }
+
+    @PostMapping(Uris.Users.LOGOUT)
+    fun logout(
+        user: AuthenticatedUser
+    ) {
+        TODO()
+    }
+
+    @GetMapping("/user")
+    fun getStatistics(@RequestParam username: String): ResponseEntity<*> {
+        return when (val res = userService.getStats(username)) {
+            is Success ->
+                ResponseEntity.status(200)
+                    .body(UserGetStatisticsOutputModel(res.value))
+
+            is Failure -> when (res.value) {
+                GetUserStatsError.NoSuchUser ->
+                    Problem.response(404, Problem.userWithUsernameNotFound)
+            }
+        }
     }
 }
