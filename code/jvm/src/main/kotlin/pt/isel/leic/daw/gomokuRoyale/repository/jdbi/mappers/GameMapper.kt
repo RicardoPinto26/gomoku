bcb813d121e0b4b08d2637aed8c94ead1d934ef1
Francisco Medeiros <a46631@alunos.isel.pt>
2023-10-18 20:51:29 +0100

    Adds game mapper and updates lobby mapper

---
+++ code/jvm/src/main/kotlin/pt/isel/leic/daw/gomokuRoyale/repository/jdbi/mappers/GameMapper.kt
@@ -0,0 +1,44 @@
+package pt.isel.leic.daw.gomokuRoyale.repository.jdbi.mappers
+
+import org.jdbi.v3.core.mapper.RowMapper
+import org.jdbi.v3.core.statement.StatementContext
+import pt.isel.leic.daw.gomokuRoyale.domain.*
+import java.sql.ResultSet
+
+class GameMapper() : RowMapper<Game> {
+    override fun map(rs: ResultSet, ctx: StatementContext): Game? {
+        val lobby = LobbyMapper().map(rs, ctx)
+        val name = rs.getString("game_name")
+        val user1 = lobby.user1
+        val user2 = lobby.user2 ?: return null
+        val settings = GameSettings(lobby.settings.boardSize, 5, Opening.FREESTYLE)
+        val board = rs.getObject("game_board").toString()
+        val winner = rs.getInt("game_winner")
+
+        val turn =
+            if (rs.getInt("game_turn") == 1) {
+                createBlackPlayer(user1)
+            } else createWhitePlayer(user2)
+
+
+        return Game(
+            name,
+            user1,
+            user2,
+            settings,
+            if (winner == 0)
+                BoardRun(
+                    //settings.boardSize,
+                    settings.winningLength,
+                    settings.overflowAllowed,
+                    turn,
+                    BlackPlayer(user1),
+                    BlackPlayer(user2),
+                    board.parseJsonToBoard()
+                )
+            else
+                BoardWin(board.parseJsonToBoard(), BlackPlayer(user1)),
+        )
+    }
+}
+
--- code/jvm/src/main/kotlin/pt/isel/leic/daw/gomokuRoyale/repository/jdbi/mappers/LobbyMapper.kt
+++ code/jvm/src/main/kotlin/pt/isel/leic/daw/gomokuRoyale/repository/jdbi/mappers/LobbyMapper.kt
@@ -9,9 +9,11 @@ import pt.isel.leic.daw.gomokuRoyale.domain.Opening
 import java.sql.Date
 import java.sql.ResultSet
 
+
 class LobbyMapper(
 ) : RowMapper<Lobby> {
     override fun map(rs: ResultSet, ctx: StatementContext): Lobby {
+        val name = rs.getString("name")
         val id = rs.getInt("id")
         val joinUserId = rs.getInt("join_user_id")
         val gridSize = rs.getInt("grid_size")
@@ -21,15 +23,18 @@ class LobbyMapper(
         val user1 = UserMapper().map(rs,"creator_user_")
         val user2 = if (joinUserId > 0) UserMapper().map(rs, "join_user_") else null
 
+        logger.info("LobbyMapper: $id, $joinUserId, $gridSize, $pointsMargin, $createdAt, $user1, $user2")
+
         //TODO create game mapper
         return Lobby(
-                id,
-                null,
-                user1,
-                user2,
-                pointsMargin,
-                Date.from(createdAt),
-                GameSettings(gridSize, 5, Opening.FREESTYLE)
+            name,
+            id,
+            null,  // TODO go go peras
+            user1,
+            user2,
+            pointsMargin,
+            Date.from(createdAt),
+            GameSettings(gridSize, 5, Opening.FREESTYLE)
         )
     }
 
